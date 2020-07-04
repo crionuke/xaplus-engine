65f3f86a6893ed35bb80356fdc849cf40da51151
Kirill Byvshev <k@byv.sh>
2020-07-04 19:44:51 +0300

    init engine for tests

--- src/test/java/com/crionuke/xaplus/XAPlusServiceTest.java
+++ src/test/java/com/crionuke/xaplus/XAPlusServiceTest.java
@@ -1,5 +1,7 @@
 package com.crionuke.xaplus;
 
+import com.crionuke.xaplus.stubs.XADataSourceStub;
+import com.crionuke.xaplus.stubs.XAPlusFactoryStub;
 import org.junit.Assert;
 
 public class XAPlusServiceTest extends Assert {
@@ -7,8 +9,18 @@ public class XAPlusServiceTest extends Assert {
     protected final int DEFAULT_TIMEOUT_S = 10;
     protected final int POLL_TIMIOUT_MS = 2000;
 
+    protected final String XA_RESOURCE_1 = "db1";
+    protected final String XA_RESOURCE_2 = "db2";
+    protected final String XA_RESOURCE_3 = "db3";
+
+    protected final String XA_PLUS_RESOURCE_1 = "service1";
+    protected final String XA_PLUS_RESOURCE_2 = "service2";
+
     protected XAPlusProperties properties;
+    protected XAPlusResources resources;
     protected XAPlusUidGenerator uidGenerator;
+    protected XAPlusThreadOfControl threadOfControl;
+    protected XAPlusEngine engine;
     protected XAPlusThreadPool threadPool;
     protected XAPlusDispatcher dispatcher;
 
@@ -18,10 +30,14 @@ public class XAPlusServiceTest extends Assert {
 
     protected void createXAPlusComponents(int defaultTimeoutInSeconds) {
         properties = new XAPlusProperties();
-        properties.setServerId("test-server");
+        properties.setServerId("server-stub");
         properties.setQueueSize(QUEUE_SIZE);
         properties.setDefaultTimeoutInSeconds(defaultTimeoutInSeconds);
-        uidGenerator = new XAPlusUidGenerator(properties);
+        resources = new XAPlusResources();
+        initResources(resources);
+        uidGenerator = new XAPlusUidGenerator();
+        threadOfControl = new XAPlusThreadOfControl();
+        engine = new XAPlusEngine(properties, dispatcher, resources, uidGenerator, threadOfControl);
         threadPool = new XAPlusThreadPool();
         dispatcher = new XAPlusDispatcher();
     }
@@ -46,5 +62,13 @@ public class XAPlusServiceTest extends Assert {
                 uidGenerator.generateUid("remote-server"));
         XAPlusTransaction transaction = new XAPlusTransaction(xid, timeoutInSeconds, properties.getServerId());
         return transaction;
+    }
+
+    private void initResources(XAPlusResources resources) {
+        resources.register(new XADataSourceStub(), XA_RESOURCE_1);
+        resources.register(new XADataSourceStub(), XA_RESOURCE_2);
+        resources.register(new XADataSourceStub(), XA_RESOURCE_3);
+        resources.register(new XAPlusFactoryStub(), XA_PLUS_RESOURCE_1);
+        resources.register(new XAPlusFactoryStub(), XA_PLUS_RESOURCE_2);
     }
 }
